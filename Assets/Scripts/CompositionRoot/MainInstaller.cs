481faadd022b8166a5654bc20ff05996efcc6f51
Andrey Kenya <andrey.kenya@mobile.dots-vendor.com>
2022-07-02 02:02:49 +0300

    While project context is being used as singleton consider to late bind monobehaviours in project installer

--- Assets/Scripts/CompositionRoot/MainInstaller.cs
+++ Assets/Scripts/CompositionRoot/MainInstaller.cs
@@ -5,7 +5,6 @@ public class MainInstaller : MonoInstaller
 {
     public override void InstallBindings()
     {
-        Container.Bind<IGameMapManager>().FromInstance(GameObject.FindObjectOfType<GameMapManager>()).AsSingle();
-        Container.Bind<IGameMapEntriesViewHelper>().FromInstance(GameObject.FindObjectOfType<GameMapEntriesViewHelper>()).AsSingle();
+        
     }
 }
--- Assets/Scripts/CompositionRoot/ProjectInstaller.cs
+++ Assets/Scripts/CompositionRoot/ProjectInstaller.cs
@@ -6,5 +6,10 @@ public class ProjectInstaller : MonoInstaller
     public override void InstallBindings()
     {
         ServicesInstaller.Install(Container);
+        
+        //TODO: refactor this! Move to scene context and prefer to use factory
+        Container.Bind<IGameMapManager>().FromMethod(() => GameObject.FindObjectOfType<GameMapManager>()).AsSingle();
+        Container.Bind<IGameMapEntriesViewHelper>().FromMethod(() => GameObject.FindObjectOfType<GameMapEntriesViewHelper>()).AsSingle();
+
     }
 }

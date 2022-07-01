f82b0b1452e0dc2f69d04c333702ac2140e7b61b
Andrey Kenya <andrey.kenya@mobile.dots-vendor.com>
2022-07-01 22:53:00 +0300

    MainInstaller added. Launcher added. Launch scene added as a main scene

---
+++ Assets/Scripts/Launch/Launcher.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using Zenject;
+
+namespace Launch
+{
+    /// <summary>
+    /// Application starting point.
+    /// </summary>
+    public class Launcher : MonoBehaviour
+    {
+        [SerializeField] private SceneContext _sceneContext;
+
+        private void Start()
+        {
+            _sceneContext.Run();
+            
+            //TODO: load resources
+            
+            SceneManager.LoadScene("Main");
+        }
+    }
+}
